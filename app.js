ed1b3018c96af671d491c33d31a42bf3924776e7
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2023-02-22 21:29:39 +0500

    Task is completed

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ let KeyN = document.getElementById('N');
 let KeyM = document.getElementById('KeyM');
 let Comma = document.getElementById('Comma');
 let Period = document.getElementById('Period');
-let ShiftRight = document.getElementById('ShiftRight');
+let Backspace = document.getElementById('Backspace');
 let Backslash = document.getElementById('Backslash');
 let Space = document.getElementById('Space');
 let Slash = document.getElementById('Slash');
@@ -47,9 +47,9 @@ let Enter = document.getElementById('Enter');
 
 let keyWordArray = [NumpadAdd, NumpadSubtract, NumpadMultiply, Equal, ControlLeft, fn, OSLeft, AltLeft, KeyQ, KeyW, KeyE, KeyR, KeyT, KeyY, KeyU, KeyI, KeyO, 
     KeyP, BracketLeft, BracketRight, KeyA, KeyS, KeyD, KeyF, KeyG, KeyH, KeyJ, KeyK, KeyL, Semicolon, Quote, ShiftLeft, KeyZ, KeyX, KeyC, KeyV, KeyB, KeyN, KeyM, 
-    Comma, Period, ShiftRight, Backslash, Space, Slash, Enter].filter(element => element != null);
+    Comma, Period, Backspace, Backslash, Space, Slash, Enter].filter(element => element != null);
 
-window.addEventListener('keydown', (e) => {
+const windowAddEventListener = () => {window.addEventListener('keydown', (e) => {
     const everyKeyword = keyWordArray.filter(element => element.id === e.code);
     if (everyKeyword.length > 0) {
         console.log(everyKeyword[0].id);
@@ -61,7 +61,9 @@ window.addEventListener('keydown', (e) => {
     } else {
         console.log('No key here');
     }
-});
+})
+};
+windowAddEventListener()
 
 let settingSVG = document.getElementById('setting-svg');    
 let moreSettings = document.querySelector('.more-settings');
@@ -69,4 +71,45 @@ let moreSettings = document.querySelector('.more-settings');
 settingSVG.onclick = () => {
     moreSettings.classList.toggle('animateShow');
     moreSettings.classList.toggle('animateHide');
+};
+
+
+
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let emailChecked = document.getElementById('email-checkbox');
+let passwordChecked = document.getElementById('password-checkbox');
+
+keyWordArray.forEach(letter => {
+    letter.addEventListener('click', () => {
+        if (emailChecked.checked == true) {
+            email.value +=  letter.attributes[1].value;
+            passwordChecked.checked = false;
+            email.focus();
+        } else if(passwordChecked.checked == true){
+            password.value +=  letter.attributes[1].value;
+            emailChecked.checked = false;
+            password.focus();
+        }
+    })
+})
+
+window.onload = function() {
+    email.value = '';
+    password.value = '';
+    passwordChecked.checked = false;
+    emailChecked.checked = false;
 }
+
+let input_hide = document.getElementById('input');
+let more_settings_hide = document.getElementById('more-settings');
+let keyboard_hide = document.getElementById('keyboard');
+let start_part = document.getElementById('start-part');
+let rule = document.getElementById('rule');
+start_part.addEventListener('click', () => {
+    more_settings_hide.classList.toggle('hide');
+    input_hide.classList.toggle('hide');
+    rule.classList.toggle('hide');
+    keyboard_hide.classList.toggle('start-hide');
+    start_part.style = 'display: none;';
+});
